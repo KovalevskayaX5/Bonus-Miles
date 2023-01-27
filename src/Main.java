7190fd71021cb30d0406ef13e5ced93554479b8b
YanaK <Yana.Koval15@yandex.ru>
2023-01-27 19:50:58 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
+public class Main {
+    public static void main(String[] args) {
+        int q = 20;
+        int price = 5_000;
+        System.out.println(price / q);
+
+    }
+}
