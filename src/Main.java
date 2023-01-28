04f26745abab20fe4833ef87e303a302d3d72757
YanaK <Yana.Koval15@yandex.ru>
2023-01-28 20:21:39 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import static java.lang.System.setOut;
 
 public class Main {
     public static void main(String[] args) {
-        int q = 20;
+        int bonus = 20;
         int price = 5_000;
-        System.out.println(price / q);
+        System.out.println((price / bonus) +"Бонусных милей");
 
     }
 }
